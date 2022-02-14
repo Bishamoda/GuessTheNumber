2f3d7ab31eb3dd4b66d5c873efab5e559e6f25e3
Bishamoda <mihailshipakin@mail.ru>
2022-02-14 23:30:12 +0300

    divided validation

--- GuessTheNumber/GuessTheNumber/Program.cs
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -11,22 +11,19 @@ string? inputNumber;
 Random randomNumber = new();
 secretNumber = randomNumber.Next(0, 999);
 
+Console.WriteLine("Кто ты воин?");
 do
 {
-    Console.WriteLine("Кто ты воин?");
     name = Console.ReadLine();
     if (String.IsNullOrWhiteSpace(name))
-    {
         Console.WriteLine($"{name} - это не строка! Андрей, ну широкую на широкую!\n");
-    }
     else
-    {
         userNameValidation = true;
-        Console.WriteLine($"Я хочу сыграть с тобой в игру, {name}!");
-        Console.WriteLine("Ты уже целый год изучаешь С#, но всё еще не постиг всех знаний ООП, паттернов и чистого кода!\n" +
-            "Предлагаю тебе выход! Просто выбери число от 0 до 999.\n");
-    }
 } while (!userNameValidation);
+
+Console.WriteLine($"Я хочу сыграть с тобой в игру, {name}!");
+Console.WriteLine("Ты уже целый год изучаешь С#, но всё еще не постиг всех знаний ООП, паттернов и чистого кода!\n" +
+    "Предлагаю тебе выход! Просто выбери число от 0 до 999.\n");
 
 while (!isWin)
 {
@@ -35,27 +32,26 @@ while (!isWin)
         count++;
         inputNumber = Console.ReadLine();
         isNumber = int.TryParse(inputNumber, out userNumber);
+
         if (!isNumber)
-        {
-            Console.WriteLine("Не ну ты фрнцуз! Это не число...\n " +
-                "Попробуй еще раз:");
-        }
+            Console.WriteLine("Не ну ты фрнцуз! Это не число...\n " + "Попробуй еще раз:");
         else
-        {
-            if (userNumber < secretNumber)
-            {
-                Console.WriteLine("Ты выбрал слишком маленькое число... Тебя заждался Delphi!\n");
-            }
-            else if (userNumber > secretNumber)
-            {
-                Console.WriteLine("Ты выбрал слишком большое число... Хочешь уйти в JS?\n");
-            }
-            else if (userNumber == secretNumber)
-            {
-                isNumber = false;
-                isWin = true;
-                Console.WriteLine($"Ты сделал правильный выбор. .Net-чики нам нужны! Смотри сколько попыток ты сделал -  {count}.");
-            }
-        }
-    } while (isNumber);
+            isNumber = true;
+    } while (!isNumber);
+
+    if (userNumber < secretNumber)
+    {
+        Console.WriteLine("Ты выбрал слишком маленькое число... Тебя заждался Delphi!\n" 
+            + "Попробуй еще раз:");
+    }
+    else if (userNumber > secretNumber)
+    {
+        Console.WriteLine("Ты выбрал слишком большое число... Хочешь уйти в JS?\n" 
+            + "Попробуй еще раз:");
+    }
+    else if (userNumber == secretNumber)
+    {
+        isWin = true;
+        Console.WriteLine($"Ты сделал правильный выбор. .Net-чики нам нужны! Смотри сколько попыток ты сделал -  {count}.");
+    }
 }
